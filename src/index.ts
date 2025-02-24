f0cb2e4fa9796b0358ec91248b5ee9db0334f80b
sora <blue.solar1999@gmail.com>
2025-02-25 07:34:56 +0900

    投稿削除APIの実装

--- src/index.ts
+++ src/index.ts
@@ -67,4 +67,19 @@ app.put("/posts/:id", async (c) => {
   return c.json(blogPosts[index]);
 });
 
+app.delete("/posts/:id", async (c) => {
+  const id = c.req.param("id");
+  const index = blogPosts.findIndex((p) => p.id == id);
+
+  if (index === -1) {
+    return c.json({ message: "Post not found" }, 404);
+  }
+
+  blogPosts = blogPosts.filter((p) => {
+    p.id !== id;
+  });
+
+  return c.json({ message: "Blog Post deleted" });
+});
+
 export default app;
