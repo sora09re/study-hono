59c297b762b0cc7b7733a5055f5bdde419b5c4f2
sora <blue.solar1999@gmail.com>
2025-02-25 07:56:10 +0900

    Basic認証の追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
+import { basicAuth } from "hono/basic-auth";
 import posts from "./posts/posts";
+import auth from "./auth/auth";
 
 export const app = new Hono();
 
 app.use("*", prettyJSON());
+app.use(
+  "/*",
+  basicAuth({
+    username: "hono",
+    password: "acoolproject",
+  })
+);
+
 app.route("/posts", posts);
+app.route("/auth", auth);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
