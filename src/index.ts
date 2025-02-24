f0f4f17f585f011ba32777509fdd3539824522da
sora <blue.solar1999@gmail.com>
2025-02-24 20:03:35 +0900

    投稿作成APIの実装

--- src/index.ts
+++ src/index.ts
@@ -40,4 +40,14 @@ app.get("/posts/:id", (c) => {
   return c.json(post);
 });
 
+app.post("/posts", async (c) => {
+  const { title, content } = await c.req.json<{
+    title: string;
+    content: string;
+  }>();
+  const newPost = { id: String(blogPosts.length + 1), title, content };
+  blogPosts = [...blogPosts, newPost];
+  return c.json(newPost, 201);
+});
+
 export default app;
