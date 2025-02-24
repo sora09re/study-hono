051d3c1baaa96cb3842526cd8c605127854dea51
sora <blue.solar1999@gmail.com>
2025-02-24 19:43:20 +0900

    ダミーデータの作成

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,24 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
+let blogPosts = [
+  {
+    id: "1",
+    title: "Blog1",
+    content: "Blog1 Content",
+  },
+  {
+    id: "2",
+    title: "Blog2",
+    content: "Blog2 Content",
+  },
+  {
+    id: "3z",
+    title: "Blog3z",
+    content: "Blog3 Content",
+  },
+];
+
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
